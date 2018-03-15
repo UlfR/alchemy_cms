5b8bd412e9f72c34e39b023744b3e54d466f43ee
Gleb Stiblo <errandir@gmail.com>
2018-03-15 12:50:11 +0600

    pre-created elements support in page creation

--- app/models/alchemy/page/page_elements.rb
+++ app/models/alchemy/page/page_elements.rb
@@ -255,7 +255,14 @@ module Alchemy
       if elements.present?
         elements.each do |element|
           next if elements_already_on_page.include?(element)
-          Element.create_from_scratch(attributes_for_element_name(element))
+          auto_element = Element.create_from_scratch(attributes_for_element_name(element))
+
+          if auto_element&.precreated_elements.present?
+            auto_element.precreated_elements.each do |name|
+              Element.create_from_scratch(name: name, page_id: self.id, parent_element_id: auto_element.id)
+            end
+          end
+
         end
       end
     end
